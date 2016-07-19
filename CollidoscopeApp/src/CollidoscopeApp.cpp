7634f99f26d385176d4e450286325558ea71dbde
Fiore Martin <f.martin@qmul.ac.uk>
2016-07-19 11:38:29 +0200

    adjusted keyboard commands

--- CollidoscopeApp/src/CollidoscopeApp.cpp
+++ CollidoscopeApp/src/CollidoscopeApp.cpp
@@ -124,74 +124,57 @@ void CollidoscopeApp::keyDown( KeyEvent event )
 {
     char c = event.getChar();
 
+    const size_t waveIdx = 0;
+
     switch (c){
     case 'r' : 
-        mAudioEngine.record( 0 );
+        mAudioEngine.record( waveIdx );
         break;
 
     case 'w': {
 
-        mWaves[0]->getSelection().setSize(mWaves[0]->getSelection().getSize() + 1);
+        mWaves[waveIdx]->getSelection().setSize(mWaves[waveIdx]->getSelection().getSize() + 1);
 
-        size_t numSelectionChunks = mWaves[0]->getSelection().getSize();
+        size_t numSelectionChunks = mWaves[waveIdx]->getSelection().getSize();
         // how many samples in one selection ?
         size_t selectionSize = numSelectionChunks * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks());
 
-        mAudioEngine.setSelectionSize(0, selectionSize);
-    };
-        break;
-
-    case 'e': {
-        mWaves[1]->getSelection().setSize(mWaves[1]->getSelection().getSize() + 1);
-
-        size_t numSelectionChunks = mWaves[1]->getSelection().getSize();
-        // how many samples in one selection ?
-        size_t selectionSize = numSelectionChunks * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks());
-
-        mAudioEngine.setSelectionSize(1, selectionSize);
+        mAudioEngine.setSelectionSize(waveIdx, selectionSize);
     };
         break;
 
     case 's': {
 
-        mWaves[0]->getSelection().setSize( mWaves[0]->getSelection().getSize() - 1 );
+        mWaves[waveIdx]->getSelection().setSize( mWaves[waveIdx]->getSelection().getSize() - 1 );
 
-        size_t selectionSize = mWaves[0]->getSelection().getSize() *(mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks());
-        mAudioEngine.setSelectionSize( 0, selectionSize );
+        size_t selectionSize = mWaves[waveIdx]->getSelection().getSize() *(mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks());
+        mAudioEngine.setSelectionSize( waveIdx, selectionSize );
     };
         break;
 
     case 'd': {
 
-        for( size_t waveIdx = 0; waveIdx < NUM_WAVES; waveIdx++){
-            size_t selectionStart = mWaves[waveIdx]->getSelection().getStart();
-            mWaves[waveIdx]->getSelection().setStart( selectionStart + 1 );
+        size_t selectionStart = mWaves[waveIdx]->getSelection().getStart();
+        mWaves[waveIdx]->getSelection().setStart( selectionStart + 1 );
 
-            selectionStart = mWaves[waveIdx]->getSelection().getStart();
-            mAudioEngine.setSelectionStart( waveIdx, selectionStart * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks()) );
-        }
+        selectionStart = mWaves[waveIdx]->getSelection().getStart();
+        mAudioEngine.setSelectionStart( waveIdx, selectionStart * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks()) );
     };
 
         break;
 
     case 'a': {
-        size_t selectionStart = mWaves[0]->getSelection().getStart();
+        size_t selectionStart = mWaves[waveIdx]->getSelection().getStart();
         
         if ( selectionStart == 0 )
             return;
 
-        mWaves[0]->getSelection().setStart( selectionStart - 1 );
+        mWaves[waveIdx]->getSelection().setStart( selectionStart - 1 );
 
-        selectionStart = mWaves[0]->getSelection().getStart();
+        selectionStart = mWaves[waveIdx]->getSelection().getStart();
 
-        mAudioEngine.setSelectionStart( 0, selectionStart * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks()) );
+        mAudioEngine.setSelectionStart( waveIdx, selectionStart * (mConfig.getWaveLen() * mAudioEngine.getSampleRate() / mConfig.getNumChunks()) );
     };
-        break;
-
-
-    case 'p':
-
-        mWaves[0]->setCursorPos(  4, mWaves[0]->getSelection().getStart(), *mDrawInfos[0] ) ;
         break;
 
     case 'f':
@@ -202,49 +185,36 @@ void CollidoscopeApp::keyDown( KeyEvent event )
         static bool isOn = false;
         isOn = !isOn;
         if ( isOn ){
-            mAudioEngine.loopOn( 0 );
-            mAudioEngine.loopOn( 1 );
+            mAudioEngine.loopOn( waveIdx );
         }
         else{
-            mAudioEngine.loopOff( 0 );
-            mAudioEngine.loopOff( 1 );
+            mAudioEngine.loopOff( waveIdx );
         }
-    };
-        break;
-
-    case 'm' :
-        mAudioEngine.setGrainDurationCoeff(0, 8);
-        break;
-
-    case 'n': {
-        mAudioEngine.setGrainDurationCoeff( 0, 1 );
     };
         break;
 
     case '9': {
-        int c = mWaves[0]->getSelection().getParticleSpread();
+        int c = mWaves[waveIdx]->getSelection().getParticleSpread();
         if ( c == 1 )
             return;
         else
             c -= 1;
 
-        mAudioEngine.setGrainDurationCoeff( 0, c );
-        mWaves[0]->getSelection().setParticleSpread( float( c ) );
-        mAudioEngine.setGrainDurationCoeff( 1, c );
-        mWaves[1]->getSelection().setParticleSpread( float( c ) );
+        mAudioEngine.setGrainDurationCoeff( waveIdx, c );
+        mWaves[waveIdx]->getSelection().setParticleSpread( float( c ) );
+
     }; break;
 
     case '0': {
-        int c = mWaves[0]->getSelection().getParticleSpread();
+        int c = mWaves[waveIdx]->getSelection().getParticleSpread();
         if ( c == 8 )
             return;
         else
             c += 1;
 
-        mAudioEngine.setGrainDurationCoeff( 0, c );
-        mWaves[0]->getSelection().setParticleSpread( float( c ) );
+        mAudioEngine.setGrainDurationCoeff( waveIdx, c );
+        mWaves[waveIdx]->getSelection().setParticleSpread( float( c ) );
     }; break;
-
     }
 
 }
